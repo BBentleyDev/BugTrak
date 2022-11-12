dc80cb02ac6f68a7a87a9bb9b64780ad19dd3d20
Brandon Bentley <bbentleydev@gmail.com>
2022-11-12 14:52:43 -0500

    added concurrently

--- server/server.js
+++ server/server.js
@@ -2,12 +2,13 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require('cors');
+const dotenv = require('dotenv').config()
 const connectDB = require("./config/database");
 const taskRoutes = require("./routes/task");
 const userRoutes = require("./routes/user");
 
 //Use .env file in config folder
-require("dotenv").config({ path: "./config/.env" });
+// require("dotenv").config({ path: "./config/.env" });
 
 //Connect To Database
 connectDB();
@@ -24,6 +25,6 @@ app.use("/tasks", taskRoutes);
 app.use("/users", userRoutes);
 
 //Server Running
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3001, () => {
   console.log("Server is running, you better catch it!");
 });
